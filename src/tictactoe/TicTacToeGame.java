beacff55a2233a9aea1217dfe0d38689394ccb4a
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-15 16:41:23 +0530

    [Prajakta]: checked for free space and if availabled maked the move

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -22,24 +22,30 @@ public class TicTacToeGame
         }
         System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
         showBoard(board);
-        int userMove = getUserMove(board);
+        getUserMove(board,UserLetter);
         showBoard(board);
+
     }
-    public static int getUserMove(char[] board)
+    public static void getUserMove(char[] board,char symbol)
     {
         Scanner sc = new Scanner(System.in);
         Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-
-        while (true)
+        Boolean ocupied=false;
+        while (ocupied==false)
         {
             System.out.println("What is your next move ? (1-9");
             int index = sc.nextInt();
             if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
-                return index;
-
+            {
+                board[index] = symbol;
+                ocupied=true;
+            }
+            else
+            {
+                System.out.println("not between 1-9 or space is not free. Again enter between 1-9");
+            }
         }
-
     }
     private static boolean isSpaceFree(char[] board, int index)
     {
