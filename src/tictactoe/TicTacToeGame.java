35d798e720c8c8377e7bf76930fceb2c8fa201aa
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-15 09:06:44 +0530

    [Prajakta]: Created class and created board method with size of array 10

---
+++ src/tictactoe/TicTacToeGame.java
@@ -0,0 +1,19 @@
+package tictactoe;
+
+public class TicTacToeGame
+{
+    public static void main(String[] args)
+    {
+        char[] board = createBoard();
+    }
+
+    public static char[] createBoard()
+    {
+        char[] board = new char[10];
+        for(int index = 0; index < board.length; index++)
+        {
+            board[index] = ' ';
+        }
+        return  board;
+    }
+}
