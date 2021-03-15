85256def5132e837e8c7bcd0208f63b0b7d53ba1
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-15 09:43:58 +0530

    [Prajakta]: create method and take userinput and check the condition using ifelse

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -1,12 +1,33 @@
 package tictactoe;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TicTacToeGame
 {
     public static void main(String[] args)
     {
         char[] board = createBoard();
-    }
+        char UserLetter = chooseLetter();
+        char ComputerLetter;
+        if(UserLetter=='X')
+        {
+            ComputerLetter='O';
+        }
+        else
+        {
+            ComputerLetter='X';
 
+        }
+        System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
+    }
+    public static char chooseLetter(){
+        char symbol;
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Enter your symbol");
+        symbol=UserInput.next().toUpperCase().charAt(0);
+        return symbol;
+    }
     public static char[] createBoard()
     {
         char[] board = new char[10];
