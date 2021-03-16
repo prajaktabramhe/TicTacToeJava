b68a28eb72a6ef5d8b06a4110ae8124e9348471e
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-16 19:40:29 +0530

    [Prajakta]: created method for Toss, Draw and Win and use condition

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -2,42 +2,159 @@ package tictactoe;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToeGame
 {
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
+        System.out.println("--------------------Welcome To TicTocToe Game-------------------------");
         char[] board = createBoard();
         char UserLetter = chooseLetter();
         char ComputerLetter;
-        if(UserLetter=='X')
+        if (UserLetter == 'X') {
+            ComputerLetter = 'O';
+        } else {
+            ComputerLetter = 'X';
+
+        }
+        System.out.println("Computer Letter is: " + ComputerLetter + " User Letter is : " + UserLetter);
+        System.out.println("Current Board:");
+        showBoard(board);
+        int first = toss();
+        boolean win=false;
+        String result=null;
+        while (win==false)
         {
-            ComputerLetter='O';
+            if (first == 1)
+            {
+                getUserMove(board, UserLetter);
+                first=2;
+            }
+            else
+                {
+                getUserMove(board, ComputerLetter);
+                first=1;
+            }
+            System.out.println("Current Board:");
+            showBoard(board);
+            result=iswin(board);
+            if(result=="X")
+            {
+                System.out.println("X is winner");
+                win=true;
+            }
+            else if(result=="O")
+            {
+                System.out.println("O is winner");
+                win=true;
+            }
+            else if(isdraw(board))
+            {
+                System.out.println("Game is draw");
+                win=true;
+            }
+
+        }
+    }
+    public static boolean isdraw(char[] board)
+    {
+        int counter=0;
+        for(int i=1;i<10;i++)
+        {
+            if(board[i]!=' ')
+            {
+                counter=counter+1;
+            }
+        }
+        if(counter==9)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static String iswin(char[] board)
+    {
+        String temp=null;
+        for (int choice = 0; choice < 8; choice++)
+        {
+            String line=null;
+
+            switch (choice)
+            {
+                case 0:
+                    line = Character.toString(board[1]) + Character.toString(board[2]) + Character.toString(board[3]);
+                    break;
+                case 1:
+                    line = Character.toString(board[4]) + Character.toString(board[5]) + Character.toString(board[6]);
+                    break;
+                case 2:
+                    line =Character.toString(board[7]) + Character.toString(board[8]) + Character.toString(board[9]);
+                    break;
+                case 3:
+                    line = Character.toString(board[1]) + Character.toString(board[4]) + Character.toString(board[7]);
+                    break;
+                case 4:
+                    line = Character.toString(board[2]) + Character.toString(board[5]) + Character.toString(board[8]);
+                    break;
+                case 5:
+                    line = Character.toString(board[3]) + Character.toString(board[6]) + Character.toString(board[9]);
+                    break;
+                case 6:
+                    line = Character.toString(board[1]) + Character.toString(board[5]) + Character.toString(board[9]);
+                    break;
+                case 7:
+                    line = Character.toString(board[3]) + Character.toString(board[5]) + Character.toString(board[7]);
+                    break;
+            }
+            //For X winner
+            if (line.equals("XXX"))
+            {
+                temp= "X";
+            }
+            // For O winner
+            else if (line.equals("OOO"))
+            {
+                temp= "O";
+            }
+            else
+            {
+                temp= "P";
+            }
+        }
+        return  temp;
+    }
+    public static int toss()
+    {
+
+        Random random=new Random();
+        int value = 1+random.nextInt(2-1+1);
+        if(value==1)
+        {
+            System.out.println("User will play first");
+            return value;
         }
         else
         {
-            ComputerLetter='X';
-
+            System.out.println("Computer will play first ");
+            return value;
         }
-        System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
-        showBoard(board);
-        int userMove = getUserMove(board);
-        showBoard(board);
     }
-    public static int getUserMove(char[] board)
+    public static void getUserMove(char[] board,char symbol)
     {
         Scanner sc = new Scanner(System.in);
         Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-
-        while (true)
+        boolean occupied=false;
+        while (occupied==false)
         {
-            System.out.println("What is your next move ? (1-9");
+            System.out.println("What is your next move ? (1-9)");
             int index = sc.nextInt();
             if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
-                return index;
-
+            {
+                board[index] = symbol;
+                occupied=true;
+            }
         }
 
     }
